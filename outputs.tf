79752586b14be43f2f1cac9047d7d985597f6029
Dyevo <ydavid99@gmail.com>
2024-11-18 00:14:14 +0000

    Correct error

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "webserver1_public_ip" {
     description = "Public IP of the webserver1"
-    value = aws_instance.webserver01.public_ip
+    value = aws_instance.Webserver01.public_ip
 }
 
 output "webserver2_public_ip" {
     description = "Private IP of the webserver2"
-    value = aws_instance.webserver02.public_ip
+    value = aws_instance.Webserver02.public_ip
 }
 
 output "rds_endpoint" {
